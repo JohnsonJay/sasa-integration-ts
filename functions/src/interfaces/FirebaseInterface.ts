aed8364bbc417a4dcccb4a6fa9a2341da49617ea
John Johnson <jpj9307@gmail.com>
2023-11-19 18:45:48 +0200

    added interface for feature layer states

---
+++ functions/src/interfaces/FirebaseInterface.ts
@@ -0,0 +1,11 @@
+export enum FeatureState {
+    add_pending = "ADD_PENDING",
+    add_success = "ADD_SUCCESS",
+    add_failure = "ADD_FAILURE",
+    update_pending = "UPDATE_PENDING",
+    update_success = "UPDATE_SUCCESS",
+    update_failure = "UPDATE_FAILURE",
+    delete_pending = "DELETE_PENDING",
+    delete_success = "DELETE_SUCCESS",
+    delete_failure = "DELETE_FAILURE"
+};
